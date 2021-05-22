d5558c04dca6d33607f290dd8bc5a80437ae5958
Yokk1e <satthabut.o@gmail.com>
2021-05-22 21:41:14 +0700

    - [feat] add nav bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
+import { Header } from "../components/Header";
 
 export default function Home() {
   return (
-    <div className="text-red-100">
-      hello next.js 
-    </div>
+    <Header/>
   )
 }
