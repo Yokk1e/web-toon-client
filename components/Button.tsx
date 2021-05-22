cd684e404c4c3ae34a31eb8838a70416554e6c6c
Yokk1e <satthabut.o@gmail.com>
2021-05-22 22:41:08 +0700

    - [feat] add button and spinner

---
+++ components/Button.tsx
@@ -0,0 +1,64 @@
+import React, {
+  ButtonHTMLAttributes,
+  DetailedHTMLProps,
+  ReactNode,
+} from "react";
+import { Spinner } from "./Spinner";
+
+const sizeClassnames = {
+  big: "py-2 px-6 text-sm",
+  small: "px-2 py-1 text-sm",
+};
+
+const colorClassnames = {
+  primary:
+    "whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900",
+  secondary:
+    "ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700",
+};
+
+export type ButtonProps = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> & {
+  size?: keyof typeof sizeClassnames;
+  color?: keyof typeof colorClassnames;
+  loading?: boolean;
+  icon?: ReactNode;
+  transition?: boolean;
+};
+
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  size = "big",
+  color = "primary",
+  disabled,
+  loading,
+  icon,
+  className = "",
+  transition,
+  ...props
+}) => {
+  return (
+    <button
+      disabled={disabled || loading}
+      className={`flex outline-none ${color} ${
+        sizeClassnames[size]
+      } ${transition ? `transition duration-200 ease-in-out` : ``} ${
+        colorClassnames[color]
+      }  flex items-center justify-center ${className}`}
+      data-testid="button"
+      {...props}
+    >
+      <span className={loading ? "opacity-0" : `flex items-center`}>
+        {icon ? <span className={`mr-2 items-center`}>{icon}</span> : null}
+        {children}
+      </span>
+      {loading ? (
+        <span className={`absolute`}>
+          <Spinner size={size === "small" ? "2" : "4"} />
+        </span>
+      ) : null}
+    </button>
+  );
+};
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,5 @@
+import { Button } from "./Button";
+
 export function Header() {
   return (
     <div className="relative bg-white">
@@ -39,18 +41,8 @@ export function Header() {
             </button>
           </div>
           <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-            <a
-              href="#"
-              className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
-            >
-              Sign in
-            </a>
-            <a
-              href="#"
-              className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-            >
-              Sign up
-            </a>
+            <Button size="small" children={`Sign in`} />
+            <Button size="small" color="secondary" children={`Sign up`} />
           </div>
         </div>
       </div>
